18b307483cbf4435ecb7413d302a648f7f7bce5d
Soly-M-27 <solymar227@icloud.com>
2021-12-10 20:10:35 +0000

    Fixed memory leaks

--- shell.c
+++ shell.c
@@ -9,6 +9,7 @@ int main(void)
 {
 	char *user_input;
 	char **tokens;
+	int x;
 
 	while (1)
 	{
@@ -20,15 +21,25 @@ int main(void)
 
 		/* if user types enter, continue prompt */
 		if (user_input[0] == '\0')
+		{
+			free(user_input);
 			continue;
+		}
 
 		/* tokenizes user_input */
 		tokens = tokenize_input(user_input);
 
 		/* if I was given a normal command */
 		if (_strcmp(tokens[0], "env") != 0)
-			execute_child(tokens);
+			x = execute_child(tokens);
 
+		if (x != 0)
+		{
+			free(user_input);
+			exit(EXIT_FAILURE);
+		}
+
+		free(tokens);
 		free(user_input);
 	}
 	return (0);
