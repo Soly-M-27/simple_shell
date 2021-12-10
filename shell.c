2151fcc479c0d3770fc949f1646740bf14a8fea3
ammartica <amisadaymartinez@gmail.com>
2021-12-10 18:51:03 +0000

    try fix mem leak

--- shell.c
+++ shell.c
@@ -18,18 +18,17 @@ int main(void)
 		/*receives user input and stores it in user_input*/
 		user_input = read_line();
 
+		/* if user types enter, continue prompt */
 		if (user_input[0] == '\0')
 			continue;
 
 		/* tokenizes user_input */
 		tokens = tokenize_input(user_input);
 
+		/* if I was given a normal command */
 		if (_strcmp(tokens[0], "env") != 0)
-		{
 			execute_child(tokens);
-		}
 
-		free_grid(tokens);
 		free(user_input);
 	}
 	return (0);
